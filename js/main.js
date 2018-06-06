4e5b01a9c76a528c678e1129c414a2ed57e045a5
Paulina Sorys <poland_paulina@yahoo.pl>
2018-06-06 19:22:49 +0200

    Add scrolling to section

--- js/main.js
+++ js/main.js
@@ -20,14 +20,26 @@
         event.preventDefault();
 
         const sectionId = event.target.getAttribute("href") || event.target.dataset.href;
-
         scrollToSection(sectionId);
       })
     })
   }
 
   const scrollToSection = sectionId => {
-
+    let sectionPosition, sectionOffset;
+    const navigationHeight = document.querySelector("header nav").offsetHeight;
+    const pageWidth = window.innerWidth;
+    if(sectionId !== "#") {
+      sectionOffset = document.querySelector(sectionId).offsetTop;
+      sectionPosition = pageWidth > mobileWidth ? (sectionOffset - navigationHeight) : sectionOffset;
+    } else {
+      sectionPosition = 0;
+    }
+    window.scrollTo({
+      'behavior': 'smooth',
+      'left': 0,
+      'top': sectionPosition
+    })
   }
 
   window.addEventListener("scroll", () => {
